09cf8d47682d8aec68628ecb51f33c853f9f6471
huhaoyu-Xeon <93701354+huhaoyu-Xeon@users.noreply.github.com>
2022-02-06 07:15:01 +0800

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
 using namespace std;
+
+int main() {
+	const int maxn = 30;
+	int num=0,t,yn=0,answer=0, a[maxn] = { 0 },b[maxn]={0};
+	cin >> num;
+	for (int i = 0; i < num; i++) {		//传入高度
+		cin >> a[i];
+		b[i] = 1;
+	}
+
+	for (int i = 0; i < num; i++) {
+		t = 0;
+		for (int j = 0; j < i; j++) {
+			if (a[j] >= a[i]&&b[j]>=t) {
+				t = b[j];				//取最大值
+				yn = 1;
+			}
+		}
+		if (yn) { b[i] = t+1; yn = 0; }
+	}
+
+	for (int i = 0; i < num; i++) {
+		if (answer < b[i]) {
+			answer = b[i];
+		}
+	}
+
+	cout << answer;
+	return 0;
+}
